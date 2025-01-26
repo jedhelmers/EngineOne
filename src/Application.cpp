a49f8875d7a362e45f4066800388fbcb349b959a
Jed Helmers <jedhelmers@gmail.com>
2025-01-25 23:25:48 -0500

    Update Application.cpp

--- src/Application.cpp
+++ src/Application.cpp
@@ -229,27 +229,23 @@ void Application::update() {
     for (unsigned int i = 0; i < shaders.size(); i++) {
         shaders[i]->Use();
 
-        glm::mat4 model = glm::mat4(1.0f);
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::mat4 projection = glm::mat4(1.0f);
+        glm::mat4 model         = glm::mat4(1.0f);
+        glm::mat4 view          = glm::mat4(1.0f);
+        glm::mat4 projection    = glm::mat4(1.0f);
 
-        // First, position the cube in the world space
-        model = glm::translate(model, cubePositions[i]);
-
-        // Then, rotate it around its own axis
-        float angle = glfwGetTime() * 50.0f;  // Speed up rotation
-        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-
-        // Set the view and projection matrices
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
+        view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+
+        model = glm::translate(model, cubePositions[i]);
+        // float angle = 20.0f * 1;
+        // model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
 
         shaders[i]->setMat4("view", view);
         shaders[i]->setMat4("model", model);
         shaders[i]->setMat4("projection", projection);
     }
 }
-
 
 void Application::render() {
     // Clear the screen
